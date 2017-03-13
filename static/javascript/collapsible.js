e7c0cd6acb86065a5e9d6cc2445eb7d05a4d8f01
Yura Kim <yuraxkim@gmail.com>
2017-03-13 19:54:12 -0400

    rework and added comments

---
+++ static/javascript/collapsible.js
@@ -0,0 +1,10 @@
+$(function() {
+    $(".collapsible-header").click(function () {
+        $header = $(this);
+        //getting the next element
+        $content = $header.next();
+        //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+        $content.slideToggle(0, function () {
+        });
+    });
+});
